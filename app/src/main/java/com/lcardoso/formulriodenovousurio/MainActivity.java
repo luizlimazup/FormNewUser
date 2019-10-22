41fbb8c8493c085e3d788e6e5dce273ca53ab52c
luizlimazup <56737969+luizlimazup@users.noreply.github.com>
2019-10-22 15:43:13 -0300

    Initial commit

---
+++ app/src/main/java/com/lcardoso/formulriodenovousurio/MainActivity.java
@@ -0,0 +1,33 @@
+package com.lcardoso.formulriodenovousurio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner school, state;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        school = (Spinner)findViewById(R.id.school);
+
+        ArrayAdapter adapter =  ArrayAdapter.createFromResource(this,
+                        R.array.school, android.R.layout.simple_spinner_item);
+        school.setAdapter(adapter);
+
+        state = (Spinner)findViewById(R.id.state);
+
+        ArrayAdapter adapterState = ArrayAdapter.createFromResource(this,
+                            R.array.state, android.R.layout.simple_spinner_item);
+        state.setAdapter(adapterState);
+
+    }
+}
